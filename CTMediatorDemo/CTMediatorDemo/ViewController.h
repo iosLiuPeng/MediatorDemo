70a04bfdba2ac6bfefb0a92abe7203f1f96606e9
iosLiuPeng <392009255@qq.com>
2019-05-27 10:48:15 +0800

    增加三种中间件（路由）

---
+++ CTMediatorDemo/CTMediatorDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CTMediatorDemo
+//
+//  Created by 刘鹏i on 2019/5/27.
+//  Copyright © 2019 wuhan.musjoy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MGJRouterURLDemo/MGJRouterURLDemo/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  MGJRouterURLDemo
+//
+//  Created by 刘鹏i on 2019/5/27.
+//  Copyright © 2019 wuhan.musjoy. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
